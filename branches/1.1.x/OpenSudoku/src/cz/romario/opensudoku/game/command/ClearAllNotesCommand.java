f4933e39453bf3a39ab0b63e924b17d7770222c4
romario333 <romario333@10b94578-05de-11de-85a1-fd18384f7567>
2010-05-30 17:07:53 +0000

    Creating 1.1.x branch.

---
+++ branches/1.1.x/OpenSudoku/src/cz/romario/opensudoku/game/command/ClearAllNotesCommand.java
@@ -0,0 +1,77 @@
+/* 
+ * Copyright (C) 2009 Roman Masek
+ * 
+ * This file is part of OpenSudoku.
+ * 
+ * OpenSudoku is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * OpenSudoku is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with OpenSudoku.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+package cz.romario.opensudoku.game.command;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cz.romario.opensudoku.game.Cell;
+import cz.romario.opensudoku.game.CellCollection;
+import cz.romario.opensudoku.game.CellNote;
+
+public class ClearAllNotesCommand implements Command {
+
+	private CellCollection mCells; 
+	private List<NoteEntry> mOldNotes = new ArrayList<NoteEntry>();
+	
+	
+	public ClearAllNotesCommand(CellCollection cells) {
+		mCells = cells;
+	}
+	
+	@Override
+	public void execute() {
+		mOldNotes.clear();
+		for (int r = 0; r < CellCollection.SUDOKU_SIZE; r++) {
+			for (int c = 0; c < CellCollection.SUDOKU_SIZE; c++) {
+				Cell cell = mCells.getCell(r, c);
+				CellNote note = cell.getNote();
+				if (!note.isEmpty()) {
+					mOldNotes.add(new NoteEntry(r, c, note));
+					cell.setNote(new CellNote());
+				}
+			}
+		}
+	}
+
+	@Override
+	public void undo() {
+		for (NoteEntry ne : mOldNotes) {
+			mCells.getCell(ne.rowIndex, ne.colIndex).setNote(ne.note);
+		}
+		
+	}
+	
+	private static class NoteEntry {
+		public int rowIndex;
+		public int colIndex;
+		public CellNote note;
+		
+		public NoteEntry(int rowIndex, int colIndex, CellNote note){
+			this.rowIndex = rowIndex;
+			this.colIndex = colIndex;
+			this.note = note;
+		}
+		
+	}
+	
+
+}
