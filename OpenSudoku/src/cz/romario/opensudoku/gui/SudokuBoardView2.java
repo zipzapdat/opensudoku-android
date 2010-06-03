bd5227d0e99cf3635f3217a5702754815da3d260
romario333 <romario333@10b94578-05de-11de-85a1-fd18384f7567>
2010-06-03 12:43:07 +0000

    SudokuBoardView2.onMeasure with test.

--- OpenSudoku/src/cz/romario/opensudoku/gui/SudokuBoardView2.java
+++ OpenSudoku/src/cz/romario/opensudoku/gui/SudokuBoardView2.java
@@ -27,52 +27,55 @@ public class SudokuBoardView2 extends FrameLayout {
 	
 	@Override
 	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-
-      
-		
-		
-		// TODO: uz jsem nejak mimo, dokoncit pak
+        int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+        int widthSize = MeasureSpec.getSize(widthMeasureSpec);
+        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+        int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+        
+        int width = -1, height = -1;
+        if (widthMode == MeasureSpec.EXACTLY) {
+        	width = widthSize;
+        } else {
+        	width = DEFAULT_BOARD_SIZE;
+        	if (widthMode == MeasureSpec.AT_MOST && width > widthSize ) {
+        		width = widthSize;
+        	}
+        }
+        if (heightMode == MeasureSpec.EXACTLY) {
+        	height = heightSize;
+        } else {
+        	height = DEFAULT_BOARD_SIZE;
+        	if (heightMode == MeasureSpec.AT_MOST && height > heightSize ) {
+        		height = heightSize;
+        	}
+        }
+        
+        if (widthMode != MeasureSpec.EXACTLY) {
+        	width = height;
+        }
+        
+        if (heightMode != MeasureSpec.EXACTLY) {
+        	height = width;
+        }
+        
+    	if (widthMode == MeasureSpec.AT_MOST && width > widthSize ) {
+    		width = widthSize;
+    	}
+    	if (heightMode == MeasureSpec.AT_MOST && height > heightSize ) {
+    		height = heightSize;
+    	}
+    	
+    	widthMeasureSpec = MeasureSpec.makeMeasureSpec(width, MeasureSpec.EXACTLY);
+    	heightMeasureSpec = MeasureSpec.makeMeasureSpec(height, MeasureSpec.EXACTLY);
+    	
+		testOnMeasure(widthMeasureSpec, heightMeasureSpec);
 		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+	}
+	
+	// intended for override in unit tests
+	protected void testOnMeasure(int widthMeasureSpec, int heightMeasureSpec) {
 		
-//        int widthMode = MeasureSpec.getMode(widthMeasureSpec);
-//        int widthSize = MeasureSpec.getSize(widthMeasureSpec);
-//        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
-//        int heightSize = MeasureSpec.getSize(heightMeasureSpec);
-//        
-//        int width = -1, height = -1;
-//        if (widthMode != MeasureSpec.EXACTLY) {
-//        	width = widthSize;
-//        } else {
-//        	width = DEFAULT_BOARD_SIZE;
-//        	if (widthMode == MeasureSpec.AT_MOST && width > widthSize ) {
-//        		width = widthSize;
-//        	}
-//        }
-//        if (heightMode == MeasureSpec.EXACTLY) {
-//        	height = heightSize;
-//        } else {
-//        	height = DEFAULT_BOARD_SIZE;
-//        	if (heightMode == MeasureSpec.AT_MOST && height > heightSize ) {
-//        		height = heightSize;
-//        	}
-//        }
-//        
-//        if (widthMode != MeasureSpec.EXACTLY) {
-//        	width = height;
-//        }
-//        
-//        if (heightMode != MeasureSpec.EXACTLY) {
-//        	height = width;
-//        }
-//        
-//    	if (widthMode == MeasureSpec.AT_MOST && width > widthSize ) {
-//    		width = widthSize;
-//    	}
-//    	if (heightMode == MeasureSpec.AT_MOST && height > heightSize ) {
-//    		height = heightSize;
-//    	}
-//        
-//        setMeasuredDimension(width, height);
 	}
 	
 	/**
