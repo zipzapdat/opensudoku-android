409e9fbac506b195970e6a22fde6fc58dd6b3069
romario333 <romario333@10b94578-05de-11de-85a1-fd18384f7567>
2010-10-22 14:54:10 +0000

    Undo stack is now persisted when activity is paused.

--- branches/1.1.x/OpenSudoku/src/cz/romario/opensudoku/game/command/AbstractCommand.java
+++ branches/1.1.x/OpenSudoku/src/cz/romario/opensudoku/game/command/AbstractCommand.java
@@ -20,19 +20,44 @@
 
 package cz.romario.opensudoku.game.command;
 
+import android.os.Bundle;
+
 /**
  * Generic interface for command in application.
  * 
  * @author romario
  *
  */
-public interface Command {
+public abstract class AbstractCommand {
+	
+	public static AbstractCommand newInstance(String commandClass) {
+		if (commandClass.equals(ClearAllNotesCommand.class.getSimpleName())) {
+			return new ClearAllNotesCommand();
+		} else if (commandClass.equals(EditCellNoteCommand.class.getSimpleName())) {
+			return new EditCellNoteCommand();
+		} else if (commandClass.equals(FillInNotesCommand.class.getSimpleName())) {
+			return new FillInNotesCommand();
+		} else if (commandClass.equals(SetCellValueCommand.class.getSimpleName())) {
+			return new SetCellValueCommand();
+		} else {
+			throw new IllegalArgumentException(String.format("Unknown command class '%s'.", commandClass));
+		}
+	}
+	
+	public String getCommandClass() {
+		return getClass().getSimpleName();
+	}
+	
 	/**
 	 * Executes the command.
 	 */
-	void execute();
+	abstract void execute();
 	/**
 	 * Undo this command.
 	 */
-	void undo();
+	abstract void undo();
+	
+	abstract void saveState(Bundle outState);
+	
+	abstract void restoreState(Bundle inState);
 }
